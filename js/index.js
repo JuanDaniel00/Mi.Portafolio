3ac4c55f0cc19603935dfa286476814404cda354
JuanDaniel00 <juanuribe@colegioguanenta.edu.co>
2024-01-31 14:17:26 -0500

    subiendo portafolio 90%

---
+++ js/index.js
@@ -0,0 +1,24 @@
+function stars() {
+    let count = 200;
+    let divBackground = document.querySelector('.divBackground');
+    let i = 0;
+    while(i<count){
+        let star= document.createElement("i");
+        let x= Math.floor(Math.random() * window.innerWidth);
+        let y= Math.floor(Math.random() * window.innerHeight - 150);
+        let duration = Math.random() * 10;
+        let size = Math.random() * 2;
+
+        star.style.left = x +'px';
+        star.style.top = y +'px';
+        star.style.width = 1+size +'px';
+        star.style.height = 1+size +'px';
+
+        star.style.animationDuration = 10 + duration +'s';
+        star.style.animationDelay = duration +'s';
+
+        divBackground.appendChild(star);
+        i++;
+    }
+}
+stars();
